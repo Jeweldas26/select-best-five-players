8e1c2752783e3828c7a1943964c3886f7cdbf3c4
Jewldas26 <jeweldas018747@gmail.com>
2023-08-09 23:10:51 +0600

    eighth commit

--- js/drawer.js
+++ js/drawer.js
@@ -1,5 +1,5 @@
 document.getElementById('cross-btn').addEventListener('click', function(){
     const selectCalculateContainer = document.getElementById('selection-calculation');
     selectCalculateContainer.classList.add('right-[-300px]');
-    document.body.classList.remove('overflow-hidden ');
+    document.body.classList.remove('overflow-hidden');
 })
